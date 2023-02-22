cee477004c44cc009213a929c8a3c10a79dc1bbc
Deniz Dogan <denizdogan@Denizs-MacBook-Pro.local>
2023-02-22 17:53:17 +0300

    write output to a file

--- pyinteraph/dccm.py
+++ pyinteraph/dccm.py
@@ -6,6 +6,8 @@ import numpy as np
 import MDAnalysis as mda
 import itertools
 
+from dat2graphml import ArgumentParserFileExtensionValidation
+
 
 class DCCMCalculation:
     selected_atoms_mapping = {
@@ -36,21 +38,34 @@ class DCCMCalculation:
                 traj_by_res[res_num][traj.frame] = np.mean(traj.positions[list(self.residues_with_atom_number.values())[res_num]])
         return traj_by_res
 
-    def compute(self):
+    def __compute(self):
         comb = list(itertools.combinations_with_replacement(list(range(0, self.n_residues)), 2))
         corr = np.zeros(shape=(self.n_residues, self.n_residues))
         for i, j in comb:
             corr[i, j] = np.corrcoef(self.coordinates_by_residue[i, :], self.coordinates_by_residue[j, :])[1][0]
         return corr
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--ref', type=str, required=True)
-parser.add_argument('--traj', type=str, required=True)
-parser.add_argument('--atoms', type=str, required=False)
-args = parser.parse_args()
+    def to_csv(self):
+        corr = self.__compute()
+        np.savetxt("dccm.csv", corr, comments='', delimiter=',', header=','.join(self.residues_with_atom_number.keys()))
 
 
-dccm = DCCMCalculation(args.ref, args.traj, args.atoms)
-corr = dccm.compute()
-print(corr)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--atoms', type=str, required=False)
+    parser.add_argument("--ref", help=".pdb reference file matrix",
+                        type=lambda file_name: ArgumentParserFileExtensionValidation((".pdb, .gro, .psf, .top, .crd"),
+                                                                                     file_name).validate_file_extension(),
+                        required=True)
+    parser.add_argument("--traj", help="a trajectory file",
+                        type=lambda file_name: ArgumentParserFileExtensionValidation((".trj, .pdb, .xtc, .dcd"),
+                                                                                     file_name).validate_file_extension(),
+                        required=True)
+    args = parser.parse_args()
+
+    dccm = DCCMCalculation(args.ref, args.traj, args.atoms)
+    dccm.to_csv()
+
+
+if __name__ == "__main__":
+    main()
